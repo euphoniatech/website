8bfce05e6ace90296e7e217f48ad423a5cebc22a
Manan Karnik <manankarnik03@gmail.com>
2022-12-31 00:08:11 +0530

    feat: add Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="flex fixed">
+      <div className="text-white text-4xl font-heading font-bold my-12 mx-16">
+        EUPHONIA TECH
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      heading: "Montserrat",
+    },
+
     extend: {},
   },
   plugins: [],
