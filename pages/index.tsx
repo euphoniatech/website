9c8e0d082ad695633a95dd077db78bd9357709b2
Manan Karnik <manankarnik03@gmail.com>
2022-12-31 23:02:50 +0530

    feat: add hero section text and button

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,21 @@ export default function Home() {
       </Head>
       <main>
         <Navbar />
-        <section className="xl:bg-[url('../public/static/images/hero-section-background.svg')] bg-contain bg-no-repeat h-screen w-full"></section>
+        <section className="xl:bg-[url('../public/static/images/hero-section-background.svg')] bg-contain bg-no-repeat flex items-center justify-end h-screen w-full">
+          <div className="w-[25%] text-right mx-12">
+            <h1 className="font-heading font-bold text-6xl">TITLE</h1>
+            <h3 className="font-heading font-light text-4xl">SUBTITLE</h3>
+            <p className="font-heading my-[4%] text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              tincidunt nulla sem. Aliquam odio odio, auctor in hendrerit nec,
+              vulputate viverra purus. Vivamus tincidunt purus ac nunc ultrices,
+              at mollis libero auctor.
+            </p>
+            <button className="bg-indigo-900 font-poppins text-yellow-200 text-xl font-bold rounded-lg px-12 py-4">
+              CLICK
+            </button>
+          </div>
+        </section>
         <section className="text-center font-heading text-4xl m-16">
           <h1>Services</h1>
         </section>
