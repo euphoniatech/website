3b45c8f8a03ab1c178a1ca2972428c33f490dc3c
Manan Karnik <manankarnik03@gmail.com>
2023-02-21 21:23:05 +0530

    feat: transparent nav at scroll top

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const useScrollPos = () => {
+  const [scrollPos, setScrollPos] = useState(0);
+  useEffect(() => {
+    const onScroll = () => setScrollPos(window.scrollY);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+  return scrollPos;
+};
+
 const Navbar = () => {
+  const scrollPos = useScrollPos();
   return (
-    <nav className="w-full flex items-center justify-between fixed my-10 px-[4%]">
+    <nav
+      className={(scrollPos == 0 ? "bg-transparent" : "").concat(
+        "bg-primary w-full flex items-center justify-between fixed py-8 px-[4%] z-10"
+      )}
+    >
       <div className="text-white text-3xl font-montserrat font-bold">
         EUPHONIA TECH
       </div>
       <div className="font-poppins flex-grow">
-        <ul className="text-lg flex items-center justify-end gap-[8%]">
+        <ul className="text-accent text-lg flex items-center justify-end gap-[6%]">
           <li>
             <a href="/about">About</a>
           </li>
